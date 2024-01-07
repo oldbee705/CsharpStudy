cbd1012dc214fc1215ef9705a3f914d784f68eee
程 张 <577834917@qq.com>
2024-01-07 15:18:06 +0800

    作业

---
+++ 核心实践/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace 核心实践
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+    
---
+++ 索引器/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace 索引器
+{
+    class IntArray
+    {
+        private int[] ints;
+
+        private int this[int index]
+        {
+            get
+            {
+                return ints[index];
+            }
+            set
+            {
+                ints[index] = value;
+            }
+        }
+
+        public void Add(int num)
+        {
+
+        }
+        public void Delete(int num)
+        {
+
+        }
+        public void Update(int lownum, int newnum)
+        {
+            if(Select(lownum) != 114514)
+            {
+                [Select(lownum)] = newnum;
+            }
+        }
+        public int Select(int num)
+        {
+            for (int i = 0; i < ints.Length; i++)
+            {
+                if (num == ints[i])
+                {
+                    return i;
+                }
+            }
+            return 114514;
+        }
+
+        public void PrintIndex(int index)
+        {
+
+            Console.WriteLine(index);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
