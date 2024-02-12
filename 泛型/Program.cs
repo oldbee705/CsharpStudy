37c72e545bb4999efa87f736a60a97024462b763
oldbee <577834917@qq.com>
2024-02-13 01:30:08 +0800

    泛型约束相关

---
+++ 泛型/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace 泛型
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Fun<int>());
+            Console.WriteLine(Fun<char>());
+            Console.WriteLine(Fun<float>());
+            Console.WriteLine(Fun<string>());
+            Console.WriteLine(Fun<double>());
+        }
+
+        static string Fun<T>()
+        {
+            if(typeof(T) == typeof(int))
+            {
+                return string.Format("{0}:{1}字节", "整形", sizeof(int));
+            }
+            else if (typeof(T) == typeof(char))
+            {
+                return string.Format("{0}:{1}字节", "字符", sizeof(char));
+            }
+            else if (typeof(T) == typeof(float))
+            {
+                return string.Format("{0}:{1}字节", "单精度浮点数", sizeof(float));
+            }
+            else if (typeof(T) == typeof(string))
+            {
+                return string.Format("{0}:{1}字节", "字符串", "?");
+            }
+            return "其他类型";
+        }
+    }
+}
---
+++ 泛型约束/Program.cs
@@ -0,0 +1,143 @@
+﻿namespace 泛型约束
+{
+    class SingleBase<T> where T : new()
+    {
+        private static T instance = new T();
+        
+        public static T Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+
+    class ArraryList<T>
+    {
+        private T[] array;
+        private int count;
+
+        public ArraryList()
+        {
+            count = 0;
+            array = new T[16];
+        }
+        public void Add(T item)
+        {
+            //判断是否需要扩容
+            if (Count >= Capacity)
+            {
+                T[] temp = new T[Capacity * 2];
+                for (int i = 0; i < Capacity; i++)
+                {
+                    temp[i] = array[i];
+                }
+                array = temp;
+            }
+            //不需要扩容
+            array[count] = item;
+            ++count;
+        }
+
+        public void Remove(T item)
+        {
+            int index = -1;
+            for (int i = 0; i < Count; i++)
+            {
+                if (array[i].Equals(item))
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            if(index != -1)
+            {
+                RemoveAt(index);
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            if(index < 0 || index >= Count)
+            {
+                Console.WriteLine("索引不合法");
+                return;
+            }
+
+            for (; index < Count - 1; index++)
+            {
+                array[index] = array[index + 1];
+            }
+            array[count - 1] = default(T);
+            --count;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= Count)
+                {
+                    Console.WriteLine("索引不合法");
+                    return default(T);
+                }
+                return array[index];
+            }
+            set
+            {
+                if (index < 0 || index >= Count)
+                {
+                    Console.WriteLine("索引不合法");
+                    return;
+                }
+                array[index] = value;
+            }
+        }
+
+        /// <summary>
+        /// 获取容量
+        /// </summary>
+        public int Capacity
+        {
+            get
+            {
+                return array.Length;
+            }
+        }
+
+        /// <summary>
+        /// 获取当前数量
+        /// </summary>
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+    }
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArraryList<int> array = new ArraryList<int>();
+            Console.WriteLine(array.Count);
+            Console.WriteLine(array.Capacity);
+            array.Add(1);
+            array.Add(2);
+            array.Add(3);
+            Console.WriteLine(array.Count);
+            Console.WriteLine(array.Capacity);
+
+            array.Remove(1);
+            array.RemoveAt(0);
+            Console.WriteLine(array[0]);
+            Console.WriteLine(array.Count);
+            array[0] = 20;
+            Console.WriteLine(array[0]);
+        }
+    }
+}
