c9a3e4ed42ffc6a33cb6ceb26b53b0d813e59635
oldbee <577834917@qq.com>
2024-02-13 22:26:42 +0800

    List和Dictionary相关

---
+++ Dictionary/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("请输入不超过三位的整数");
+
+                Console.WriteLine(GetInfo(int.Parse(Console.ReadLine())));
+            }
+            catch
+            {
+                Console.WriteLine("请输入合法的数字");
+            }
+            string str = "Welcome to Unity World!";
+            GetCount(str);
+        }
+
+        static string GetInfo(int num)
+        {
+            Dictionary<int, string> dic = new Dictionary<int, string>();
+            dic.Add(0, "零");
+            dic.Add(1, "壹");
+            dic.Add(2, "贰");
+            dic.Add(3, "叁");
+            dic.Add(4, "肆");
+            dic.Add(5, "伍");
+            dic.Add(6, "陆");
+            dic.Add(7, "柒");
+            dic.Add(8, "捌");
+            dic.Add(9, "玖");
+            dic.Add(10, "拾");
+            string str = "";
+            int b = num / 100;
+            int s = num % 100 / 10;
+            int g = num % 10;
+            if (b != 0)
+            {
+                str += dic[b];
+            }
+            if( s != 0 || str != "")
+            {
+                str += dic[s];
+            }
+            
+            str += dic[g];
+            return str;
+        }
+
+        static void GetCount(string str)
+        {
+            Dictionary<char, int> dic = new Dictionary<char, int>();
+            str = str.ToLower(); 
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (dic.ContainsKey(str[i]))
+                {
+                    dic[str[i]] += 1;
+                }
+                else
+                {
+                    dic.Add(str[i], 1);
+                }
+            }
+
+            foreach(char item in dic.Keys)
+            {
+                Console.WriteLine("{0}出现了{1}次",item, dic[item]);
+            }
+        }
+    }
+}
---
+++ List/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace List
+{
+    abstract class Monster
+    {
+        public static List<Monster> monsters = new List<Monster>();
+        public Monster()
+        {
+            monsters.Add(this);
+        }
+
+        public abstract void Atk();
+    }
+
+    class Boss : Monster
+    {
+        public override void Atk()
+        {
+            Console.WriteLine("Boss攻击");
+        }
+    }
+
+    class Gablin : Monster
+    {
+        public override void Atk()
+        {
+            Console.WriteLine("哥布林攻击");
+        }
+    }
+    //List可以自定义存储类型，ArrayList以Object类型存储，会使用装箱拆箱从而导致占用更多内存
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            List<string> list2 = new List<string>();
+            List<bool> list3 = new List<bool>();
+
+
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Remove(1);
+            list.RemoveAt(1);
+            list[0] = 20;
+            Console.WriteLine(list[0]);
+            Console.WriteLine("************************");
+
+            List<int> ints = new List<int>();
+            for (int i = 10; i > 0; i--)
+            {
+                ints.Add(i);
+            }
+            ints.RemoveAt(4);
+            for (int i = 0; i < ints.Count; i++)
+            {
+                Console.WriteLine(ints[i]);
+            }
+            Console.WriteLine("************************");
+
+            Boss b1 = new Boss();
+            Boss b2 = new Boss();
+            Gablin g1 = new Gablin();
+            Gablin g2 = new Gablin();
+
+            for(int i = 0; i < Monster.monsters.Count; i++)
+            {
+                Monster.monsters[i].Atk();
+            }
+            
+            foreach(Monster monster in Monster.monsters) 
+            { 
+                monster.Atk(); 
+            }
+
+        }
+    }
+}
