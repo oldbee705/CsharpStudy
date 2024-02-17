500efb5a315c826f677d02cc11eae8404debf7c1
oldbee <577834917@qq.com>
2024-02-17 17:32:21 +0800

    Lambad表达式相关

---
+++ lambad表达式/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace lambad表达式
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TestAction()();
+        }
+
+        static Action TestAction()
+        {
+            Action action = null;
+            for (int i = 1; i < 11; i++)
+            {
+                int index = i;
+                action += () =>
+                {
+                    Console.WriteLine(index);
+                };
+            }
+            return action;
+        }
+    }
+}
