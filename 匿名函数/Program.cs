78230a199f61da95e05df2427c1f6ac130f43215
oldbee <577834917@qq.com>
2024-02-17 15:36:56 +0800

    匿名函数相关

---
+++ 匿名函数/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace 匿名函数
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TestFun(5)(5));
+        }
+
+        static Func<int, int> TestFun(int i)
+        {
+            return delegate (int j)
+            {
+                return i * j;
+            };
+        }
+    }
+}
