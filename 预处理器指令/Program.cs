cd5cfdfb3b37d00451a76f9a6d2adf5b6e737de9
oldbee <577834917@qq.com>
2024-02-19 00:17:41 +0800

    预处理器指令相关

---
+++ 预处理器指令/Program.cs
@@ -0,0 +1,30 @@
+﻿#define Unity5
+#define Unity2017
+#define Unity2020
+namespace 预处理器指令
+{
+    //#region #endregion
+    //#define #undef 
+    //#if #elif #else #endif
+    //#error #warning
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static int Calc(int a, int b)
+        {
+#if Unity5
+            return a + b;
+#elif Unity2017
+            return a * b;
+#elif Unity2020
+            return a - b;
+#else
+            return 0;
+#endif
+        }
+    }
+}
