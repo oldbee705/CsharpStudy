d4f86e33d6bee8e0d4eed766434c4af7d14f553e
oldbee <577834917@qq.com>
2024-02-24 23:35:56 +0800

    反射相关

---
+++ 反射/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+namespace 反射
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.LoadFrom(@"F:\Csharp\Git\oldbee705\CsharpStudy\反射类库\bin\Debug\net8.0\反射类库");
+            Type[] types = assembly.GetTypes();
+            for (int i = 0; i < types.Length; i++)
+            {
+                Console.WriteLine(types[i]);
+            }
+            Type player = assembly.GetType("反射类库.Player");
+            object obj = Activator.CreateInstance(player);
+            Console.WriteLine(obj);
+        }
+    }
+}
---
+++ 反射类库/Class1.cs
@@ -0,0 +1,23 @@
+﻿namespace 反射类库
+{
+    public struct Position
+    {
+        public int x;
+        public int y;
+
+        public Position() { }
+    }
+    public class Player
+    {
+        public string Name;
+        public int hp;
+        public int atk;
+        public int def;
+        public Position pos;
+
+        public override string ToString()
+        {
+            return string.Format("hp:{0} atk:{1} def:{2}", hp, atk, def);
+        }
+    }
+}
